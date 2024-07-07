0c5575d14ea0fed212460b9dc45a276ebc91675f
Jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-07-07 07:31:11 +0300

    speedometer

--- speed.js
+++ speed.js
@@ -1,1 +1,20 @@
-gdfd
+
+
+function mySpeed(speed) {
+    let speedLimit = 70;
+//the if statements
+    if (speed <= speedLimit) {
+        document.getElementById("book").innerHTML = "OK";
+    } else {
+        const demeritPoints = Math.floor((speed - speedLimit) / 5);
+        document.getElementById("book").innerHTML = `Demerit points: ${demeritPoints}`;
+        console.log(`Points: ${demeritPoints}`);
+
+        if (demeritPoints > 12) {
+            document.getElementById("book").innerHTML = "Your license has been suspended";
+        }
+    }
+}
+
+const speed = parseInt(prompt("Enter the speed of the car: "));
+mySpeed(speed);
